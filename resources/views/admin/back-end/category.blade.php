b8f76c3a037462091eb76eecc95d8f51199d8f3c
JonesKhaii <duongkhaipham@gmail.com>
2024-05-17 03:31:23 +0700

    Update delete function for all

--- resources/views/admin/back-end/category.blade.php
+++ resources/views/admin/back-end/category.blade.php
@@ -12,8 +12,7 @@
                         <div class="title-header option-title">
                             <h5>All Category</h5>
                             <form class="d-inline-flex">
-                                <a href="{{ route('addNewCategory') }}"
-                                   class="align-items-center btn btn-theme d-flex">
+                                <a href="{{ route('addNewCategory') }}" class="align-items-center btn btn-theme d-flex">
                                     <i data-feather="plus-square"></i>Add New
                                 </a>
                             </form>
@@ -29,29 +28,22 @@
                                         <th>Option</th>
                                     </tr>
                                     </thead>
-
                                     <tbody>
                                     @foreach($categories as $category)
                                     <tr>
                                         <td>{{ $category->name }}</td>
-
                                         <td>
                                             <div class="category-icon">
                                                 <img src="{{ asset('back-end/assets/images/store-icons/' . $category->icon_url) }}" class="img-fluid" alt="">
-
                                             </div>
                                         </td>
-
                                         <td>
                                             <ul>
-
                                                 <li>
                                                     <a href="{{ route('editCategory', ['id' => $category->id]) }}">
                                                         <i class="ri-pencil-line"></i>
                                                     </a>
                                                 </li>
-
-
                                                 <li>
                                                     <a href="#" class="delete-category" data-bs-toggle="modal" data-bs-target="#exampleModalToggle" data-id="{{ $category->id }}">
                                                         <i class="ri-delete-bin-line"></i>
@@ -71,80 +63,86 @@
         </div>
     </div>
     <!-- All Category Table Ends-->
-<!-- Delete Modal Box Start -->
-<div class="modal fade theme-modal remove-category" id="exampleModalToggle" aria-hidden="true" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header d-block text-center">
-                <h5 class="modal-title w-100" id="exampleModalLabel22">Are You Sure?</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                    <i class="fas fa-times"></i>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="remove-box">
-                    <p>The permission for the use/group, preview is inherited from the object, object will create a
-                        new permission for this object</p>
+
+    <!-- Delete Modal Box Start -->
+    <div class="modal fade theme-modal remove-category" id="exampleModalToggle" aria-hidden="true" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header d-block text-center">
+                    <h5 class="modal-title w-100" id="exampleModalLabel22">Are You Sure?</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                        <i class="fas fa-times"></i>
+                    </button>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-animation btn-md fw-bold" data-bs-dismiss="modal">No</button>
-                <form id="delete-form" method="POST" action="">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-animation btn-md fw-bold">Yes</button>
-                </form>
-            </div>
-
-        </div>
-    </div>
-</div>
-
-<div class="modal fade theme-modal remove-coupon" id="exampleModalToggle2" aria-hidden="true" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title text-center" id="exampleModalLabel12">Done!</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                    <i class="fas fa-times"></i>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="remove-box text-center">
-                    <div class="wrapper">
-                        <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
-                            <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
-                            <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
-                        </svg>
+                <div class="modal-body">
+                    <div class="remove-box">
+                        <p>The permission for the use/group, preview is inherited from the object, object will create a new permission for this object</p>
                     </div>
-                    <h4 class="text-content">It's Removed.</h4>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-dismiss="modal">Close</button>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-animation btn-md fw-bold" data-bs-dismiss="modal">No</button>
+                    <form id="delete-form" method="POST" action="">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-animation btn-md fw-bold">Yes</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+    <!-- Delete Modal Box End -->
 
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        const deleteButtons = document.querySelectorAll('.delete-category');
-        deleteButtons.forEach(button => {
-            button.addEventListener('click', function () {
-                const categoryId = this.getAttribute('data-id');
-                const deleteForm = document.getElementById('delete-form');
-                deleteForm.action = `/category/${categoryId}`;
+    <!-- Success Modal Box Start -->
+    <div class="modal fade theme-modal remove-coupon" id="exampleModalToggle2" aria-hidden="true" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-center" id="exampleModalLabel12">Done!</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="remove-box text-center">
+                        <div class="wrapper">
+                            <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                                <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
+                                <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
+                            </svg>
+                        </div>
+                        <h4 class="text-content">It's Removed.</h4>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Success Modal Box End -->
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const deleteButtons = document.querySelectorAll('.delete-category');
+            deleteButtons.forEach(button => {
+                button.addEventListener('click', function () {
+                    const categoryId = this.getAttribute('data-id');
+                    const deleteForm = document.getElementById('delete-form');
+                    deleteForm.action = `/category/${categoryId}`;
+                });
             });
+
+            // Kiểm tra nếu có session success thì hiển thị modal thành công
+            @if(session('success'))
+            const successModal = new bootstrap.Modal(document.getElementById('exampleModalToggle2'));
+            successModal.show();
+            // Xóa session success sau khi hiển thị modal để tránh hiển thị lại khi tải lại trang
+            @php
+                Session::forget('success');
+            @endphp
+            @endif
         });
-        @if(session('success'))
-        const successModal = new bootstrap.Modal(document.getElementById('exampleModalToggle2'));
-        successModal.show();
-    @endif
-    });
-</script>
-<!-- Delete Modal Box End -->
+    </script>
 <!-- Container-fluid end -->
 
 @endsection
-
--- routes/web.php
+++ routes/web.php
@@ -143,7 +143,7 @@ Route::get('/product-category/{id}', [ProductController::class, 'listProductCate
 Route::get('/product-brand/{id}', [ProductController::class, 'listProductBrand'])->name('listProductBrand');
 Route::get('/product-category/7', [ProductController::class, 'listService'])->name('listService');
 Route::get('/product-category', [ProductController::class, 'listProduct'])->name('listProduct');
-
+Route::delete('/products/{id}', [productController::class, 'softDelete'])->name('softDeleteProduct');
 
 
 
