77c53e7ddec5edcef5f100cdb984fb174fce9fec
jaycyisme <quoctuanisme0610@gmail.com>
2024-05-19 22:47:42 +0700

    search function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use App\Models\Brand;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
@@ -18,12 +19,22 @@ class HomeController extends Controller
                         ->orWhere('category_id', 4)
                         ->get();
         $toy_products = Product::where('category_id', 5)
+                        ->where('is_active', 1)
                         ->get();
         $pharmacy_products = Product::where('category_id', 6)
+                         ->where('is_active', 1)
                         ->get();
         $service_products = Product::where('category_id', 7)
+                        ->where('is_active', 1)
                         ->get();
 
         return view('.petshop.fastkart.front-end.index', compact('categories', 'products', 'food_products', 'brands', 'food_products', 'toy_products', 'pharmacy_products', 'service_products'));
+    }
+
+    public function search(Request $request) {
+        $keywords = $request->keywords_submit;
+        $search_product = Product::where('name', 'like', '%'.$keywords.'%')->get();
+
+        return view('.petshop.fastkart.front-end.search', compact('keywords', 'search_product'));
     }
 }
